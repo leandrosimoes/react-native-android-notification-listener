0155032495d2fead0c6764ec638f106baca4a388
Arun Ahuja <arunahuja94@gmail.com>
2021-05-30 16:33:24 +0530

    removed deprecated extras key for small icon and added support for large icon

--- android/src/main/java/com/lesimoes/androidnotificationlistener/RNNotification.java
+++ android/src/main/java/com/lesimoes/androidnotificationlistener/RNNotification.java
@@ -34,7 +34,7 @@ public class RNNotification {
     protected String icon;
     protected String image;
     protected String time;
-
+    protected String iconLarge;
     public RNNotification(Context context, StatusBarNotification sbn) {
         Notification notification = sbn.getNotification();
 
@@ -52,7 +52,7 @@ public class RNNotification {
             this.audioContentsURI = this.getPropertySafely(notification, Notification.EXTRA_AUDIO_CONTENTS_URI);
             this.imageBackgroundURI = this.getPropertySafely(notification, Notification.EXTRA_BACKGROUND_IMAGE_URI);
             this.extraInfoText = this.getPropertySafely(notification, Notification.EXTRA_INFO_TEXT);
-
+            this.iconLarge = this.getNotificationLargeIcon(context, notification, packageName);
             this.icon = this.getNotificationIcon(context, notification, packageName);
             this.image = this.getNotificationImage(notification);
             this.groupedMessages = this.getGroupedNotifications(notification);
@@ -96,11 +96,9 @@ public class RNNotification {
 
     private String getNotificationIcon(Context context, Notification notification, String packageName) {
         try {
-            int iconId = notification.extras.getInt(Notification.EXTRA_SMALL_ICON);
-
+           
             String result = "";
-
-            if (iconId <= 0) {
+ 
                 Icon iconInstance = notification.getSmallIcon();
                 Drawable iconDrawable = iconInstance.loadDrawable(context);
                 Bitmap iconBitmap = ((BitmapDrawable) iconDrawable).getBitmap();
@@ -109,18 +107,30 @@ public class RNNotification {
                 iconBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
 
                 result = Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
-            } else {
-                PackageManager manager = context.getPackageManager();
-                Resources resources = manager.getResourcesForApplication(packageName);
+            
 
-                Drawable iconDrawable = resources.getDrawable(iconId);
+            return TextUtils.isEmpty(result) ? result : "data:image/png;base64," + result;
+        } catch (Exception e) {
+            Log.d(TAG, e.getMessage());
+            return "";
+        }
+    }
+    
+    
+    private String getNotificationLargeIcon(Context context, Notification notification, String packageName) {
+        try {
+            
+            String result = "";
+ 
+                Icon iconInstance = notification.getLargeIcon();
+                Drawable iconDrawable = iconInstance.loadDrawable(context);
                 Bitmap iconBitmap = ((BitmapDrawable) iconDrawable).getBitmap();
 
                 ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
                 iconBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
 
                 result = Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
-            }
+             
 
             return TextUtils.isEmpty(result) ? result : "data:image/png;base64," + result;
         } catch (Exception e) {
